d36fe8f7a05fc7f062a0bc6980bc38decbdb3dc7
Parimarjan Negi <pnegi@stanford.edu>
2018-02-02 01:54:35 -0800

    stuff with crossings

--- visual_pygame.py
+++ visual_pygame.py
@@ -10,6 +10,8 @@ colour = (0,0,255)
 STEP_SIZE = 0.5
 # STEP_SIZE = 1
 RANDOM_MOVEMENT = False
+NO_CROSSINGS = False
+NUM_POINTS = 20
 
 def normalize(v):
     norm = np.linalg.norm(v)
@@ -40,10 +42,42 @@ def init(n=4, coords=None):
     '''
     points = []
     for i in range(n):
+        print('i at start = ', i)
         if coords:
             points.append(Point(coord=coords[i]))
         else:
-            points.append(Point())
+            if NO_CROSSINGS:
+                # Only add p2 if it doesn't cause crossings with points so far
+                num_try = 0
+                while True:
+                    num_try += 1
+                    p2 = Point()
+                    if (i <= 1): 
+                        # add 2 points always. Third point onwards be careful.
+                        points.append(Point())
+                        break
+                    found_good_point = True
+                    # check every point until i.
+                    for j in range(i):
+                        if (j < 1):
+                            continue
+                        if (intersect(points[j-1].pos, points[j].pos,
+                            points[i-1].pos, p2.pos) and num_try <= 1000):
+                            found_good_point = False
+                            break 
+
+                    if found_good_point:
+                        print('found good point = ', i)
+                        break
+
+                    # if (intersect(points[i-2].pos, points[i-1].pos,
+                        # points[i-1].pos, p2.pos)):
+                        # break 
+
+                if (i > 1):
+                    points.append(p2)
+            else:
+                points.append(Point())
 
     # adding the following code.
     for i in range(n):
@@ -83,16 +117,47 @@ def draw_points(points):
          p.circle = pygame.draw.circle(screen, colour, (int(p.pos[0]),
              int(p.pos[1])), size, 0)
 
-pygame.init()
-screen = pygame.display.set_mode((640, 480))
-clock = pygame.time.Clock()
+def ccw(A,B,C):
+    return (C[1]-A[1]) * (B[0]-A[0]) > (B[1]-A[1]) * (C[0]-A[0])
+
+# Return true if line segments AB and CD intersect
+def intersect(A,B,C,D):
+    return ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D)
+def calculate_crossings(points):
+    crossings = 0
+    for i, p in enumerate(points):
+
+        cur_dist = np.linalg.norm(p.pos - p.following.pos)
+        # don't count for crossings
+        if (cur_dist <= 7): continue
+
+        for j, _ in enumerate(points):
+            if (i == j): continue
+            cur_pt = points[i]
+            next_pt = points[(i+1) % len(points)]
+
+            cur_pt2 = points[(i+1) % len(points)]
+            next_pt2 = points[(i+2) % len(points)]
+            
+            if intersect(cur_pt.pos, next_pt.pos, cur_pt2.pos, next_pt2.pos):
+                crossings += 1
+
+    return crossings
+
 
 # coords = [(200.00,200.00), (400.00,400.00), (200.00, 400.00), (400.00, 200.00)]
 # coords = [(200.00,200.00), (200.00, 400.00), (400.00, 400.00), (400.00,200.00)]
 # points = init(n=len(coords), coords=coords)
 # points = init()
-points = init(n=1000)
-file_num = 0
+
+pygame.init()
+screen = pygame.display.set_mode((640, 480))
+clock = pygame.time.Clock()
+points = init(n=NUM_POINTS)
+i = 0
+
+crossings = calculate_crossings(points)
+print('total crossings = ', crossings)
 
 def total_dist(points):
     cur_dist = 0
@@ -105,22 +170,36 @@ while True:
         if event.type == pygame.QUIT:
             break
     # FFS figure out how to save frames.
-    filename = "Snaps/%04d.png" % file_num
+    filename = "Snaps/%04d.png" % i
     pygame.image.save(screen, filename)
 
     screen.fill(pygame.Color("black"))
+
     draw_points(points)
     update(points)
     pygame.display.flip()
-    file_num += 1
 
-    if (total_dist(points) < 10):
-        print('converged!')
+    i += 1 
+    if i % 100 == 0:
+        print(i)
+        d = total_dist(points)
+        print('{} : {}'.format(i, d))
+        # crossings = calculate_crossings(points)
+        # print('total crossings = ', crossings)
+        if (d < 20):
+            print('converged')
+            print('i = ', i)
+            break
 
-    print(total_dist(points))
+        # if (crossings == 0):
+            # print('crossings = 0')
+            # print('i = ', i)
+            # break
 
-    # if file_num % 1000 == 0:
-        # STEP_SIZE = STEP_SIZE / 5
+    # if i % 1000 == 0 and STEP_SIZE > 0.01:
+        # STEP_SIZE = STEP_SIZE / 2
         # print("new STEP SIZE = ", STEP_SIZE)
+
+
 
 # os.system("avconv -r 8 -f image2 -i Snaps/%04d.png -y -qscale 0 -s 640x480 -aspect 4:3 result.avi")
